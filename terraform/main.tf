449ba6df421279db97394758b6e7c89b8e5a4091
netserf <networkserf@gmail.com>
2022-03-28 14:07:27 -0700

    main.tf and variables.tf updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,47 @@ terraform {
 provider "google" {
   // TODO: Add your GCP credentials, if outside Cloud Shell
 }
+
+data "google_compute_network" "net" {
+  name = var.network_name
+}
+
+data "google_compute_subnetwork" "subnet" {
+  name          = var.subnet_name
+  network       = google_compute_network.net.id
+}
+
+output "subnet_region" {
+  value = data.google_compute_subnetwork.subnet.region
+}
+
+output "network_id" {
+  value = data.google_compute_network.net.id
+}
+
+output "router_name" {
+  value = data.google_compute_router.router.name
+}
+
+output "router_region" {
+  value = data.google_compute_router.router.region
+}
+
+#resource "google_compute_router" "router" {
+#  name    = var.router_name
+#  region  = google_compute_subnetwork.subnet.region
+#  network = google_compute_network.net.id
+#}
+
+#resource "google_compute_router_nat" "nat" {
+#  name                               = var.nat_name
+#  router                             = google_compute_router.router.name
+#  region                             = google_compute_router.router.region
+#  nat_ip_allocate_option             = "AUTO_ONLY"
+#  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+#
+#  log_config {
+#    enable = true
+#    filter = "ERRORS_ONLY"
+#  }
+#}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,19 @@
-variable "project_id" {
-  default = "{PROJECT_ID}"
+variable "subnet_region" {
+  default = "northamerica-northeast1"
+}
+
+variable "network_name" {
+  default = "csp-vpc1"
+}
+
+variable "subnet_name" {
+  default = "csp-vpc1-subnet"
+}
+
+variable "router_name" {
+  default = "csp-vpc1-nat-router"
+}
+
+variable "nat_name" {
+  default = "csp-vpc1-nat"
 }
